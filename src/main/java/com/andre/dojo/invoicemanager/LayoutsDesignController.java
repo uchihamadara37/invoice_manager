5494b43933b22d58b1753012a759c02de47866af
uchihamadara37 <andreamusholin@gmail.com>
2024-08-03 09:24:34 +0700

    okoko

--- src/main/java/com/andre/dojo/invoicemanager/LayoutsDesignController.java
+++ src/main/java/com/andre/dojo/invoicemanager/LayoutsDesignController.java
@@ -15,9 +15,7 @@ import net.sf.jasperreports.export.SimpleExporterInput;
 import net.sf.jasperreports.export.SimpleGraphics2DExporterOutput;
 
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
@@ -41,30 +39,67 @@ public class LayoutsDesignController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        try {
-            InputStream reportStream = getClass().getResourceAsStream("Jrxml/sample.jrxml");
-            JasperReport jasperReport = JasperCompileManager.compileReport("/home/andre/Documents/invoiceManager/src/main/resources/com/andre/dojo/invoicemanager/Jrxml/sample.jrxml");
-            Map<String, Object> parameters = new HashMap<>();
-            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null, new JREmptyDataSource());
+//        try {
+//            InputStream reportStream = getClass().getResourceAsStream("Jrxml/sample.jrxml");
+//            JasperReport jasperReport = JasperCompileManager.compileReport("/home/andre/Documents/invoiceManager/src/main/resources/com/andre/dojo/invoicemanager/Jrxml/sample.jrxml");
+//            Map<String, Object> parameters = new HashMap<>();
+//            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null, new JREmptyDataSource());
+//
+//            BufferedImage image = new BufferedImage(595, 842, BufferedImage.TYPE_INT_RGB);
+//            JRGraphics2DExporter exporter = new JRGraphics2DExporter();
+//            exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+//
+//            SimpleGraphics2DExporterOutput output = new SimpleGraphics2DExporterOutput();
+//            output.setGraphics2D(image.createGraphics());
+//            exporter.setExporterOutput(output);
+//
+//            exporter.exportReport();
+//
+//            // Convert BufferedImage to JavaFX Image
+//            javafx.scene.image.Image fxImage = convertToFXImage(image);
+//
+//            ImageView imageView = new ImageView(fxImage);
+//            anchorPanePreview.getChildren().add(imageView);
+//        } catch (JRException e) {
+//            throw new RuntimeException(e);
+//        }
+        InputStream reportStream = getClass().getResourceAsStream("Jrxml/sample.jrxml");
+//        System.out.println(getClass().getResource("Jrxml/sample.jrxml"));
+        File reportFile = new File(getClass().getResource("Jrxml/sample.jrxml").getFile());
+        System.out.println(reportFile);
 
-            BufferedImage image = new BufferedImage(595, 842, BufferedImage.TYPE_INT_RGB);
-            JRGraphics2DExporter exporter = new JRGraphics2DExporter();
-            exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+        if (reportStream == null){
+            System.out.println("jrxml masih kosong");
+        }else{
+            System.out.println("jrxml ada isinya");
 
-            SimpleGraphics2DExporterOutput output = new SimpleGraphics2DExporterOutput();
-            output.setGraphics2D(image.createGraphics());
-            exporter.setExporterOutput(output);
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(reportStream))) {
+                String line;
+                StringBuilder str = new StringBuilder();
+                while ((line = reader.readLine()) != null) {
+                    str.append(line);
+                    str.append("\n");
+                }
+                textJrxml.setText(str.toString());
+            } catch (IOException e) {
+                System.out.println("Error membaca file: " + e.getMessage());
+            }
+            // show and compile from jasper file.
+            try {
+                // Kompilasi JRXML menjadi JasperReport
+                JasperReport jasperReport = JasperCompileManager.compileReport(reportStream);
+                System.out.println("JRXML berhasil dikompilasi menjadi JasperReport");
 
-            exporter.exportReport();
+                // Opsional: Simpan JasperReport ke file .jasper
+                JasperCompileManager.compileReportToFile("Jrxml/sample.jrxml", "output/sample.jasper");
+                System.out.println("JasperReport berhasil disimpan ke file .jasper");
 
-            // Convert BufferedImage to JavaFX Image
-            javafx.scene.image.Image fxImage = convertToFXImage(image);
+            } catch (JRException e) {
+                System.out.println("Error saat mengompilasi JRXML: " + e.getMessage());
 
-            ImageView imageView = new ImageView(fxImage);
-            anchorPanePreview.getChildren().add(imageView);
-        } catch (JRException e) {
-            throw new RuntimeException(e);
+            }
         }
+//        System.out.println(System.getProperty("java.class.path"));
     }
 
     public static javafx.scene.image.Image convertToFXImage(BufferedImage image) {
