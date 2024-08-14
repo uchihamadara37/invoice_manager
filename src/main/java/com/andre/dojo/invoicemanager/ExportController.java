954ae0c58555dc87d0017fc9dfb49c037af8188e
andre <andreamusholin@gmail.com>
2024-08-14 10:06:06 +0700

    halaman udah bisa digeser

---
+++ src/main/java/com/andre/dojo/invoicemanager/ExportController.java
@@ -0,0 +1,4 @@
+package com.andre.dojo.invoicemanager;
+
+public class ExportController {
+}
--- src/main/java/com/andre/dojo/invoicemanager/HelloController.java
+++ src/main/java/com/andre/dojo/invoicemanager/HelloController.java
@@ -8,10 +8,14 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 import java.net.URL;
@@ -30,26 +34,54 @@ public class HelloController implements Initializable {
     @FXML
     private TableColumn<Invoice, String> tableColumnCustomer;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        openDashboardPage();
-    }
 
-    private void openDashboardPage() {
-        try{
-            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Dashboard-view.fxml"));
-            Scene scene = new Scene(fxmlLoader.load());
-            HelloApplication.getMainStage().setTitle("Authentication!");
-            HelloApplication.getMainStage().setScene(scene);
-        }catch (IOException e1){
-            e1.printStackTrace();
-        }
-    }
+    @FXML
+    private Pane tombolInvoice;
+    @FXML
+    private Pane tombolChangeData;
+    @FXML
+    private Pane tombolExport;
+
+    @FXML
+    private AnchorPane anchorPaneMain;
+    @FXML
+    private Pane paneInvoice;
+
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("jangkrik");
 
+        loadTableView();
+
+        // event tombol
+        tombolChangeData.setOnMouseClicked(e -> {
+            openChangeDataPane();
+        });
+        tombolInvoice.setOnMouseClicked(e -> {
+            openInvoicePane();
+        });
+        tombolExport.setOnMouseClicked(e -> {
+            openExportPane();
+        });
+
+    }
+
+    private void openExportPane() {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("export-view.fxml"));
+            AnchorPane anchorPane = new AnchorPane((Node) fxmlLoader.load());
+            anchorPaneMain.getChildren().removeFirst();
+            anchorPaneMain.getChildren().add(anchorPane);
+        }catch (IOException e1){
+            e1.printStackTrace();
+        }
+    }
+
+    private void openInvoicePane() {
+        anchorPaneMain.getChildren().removeFirst();
+        anchorPaneMain.getChildren().add(paneInvoice);
         loadTableView();
     }
 
@@ -62,5 +94,16 @@ public class HelloController implements Initializable {
         tableColumnCode.setCellValueFactory(e -> new SimpleStringProperty(e.getValue().getInvoiceCode()));
         tableColumnDescription.setCellValueFactory(e -> new SimpleStringProperty(e.getValue().getDescription()));
         tableColumnCustomer.setCellValueFactory(e -> new SimpleStringProperty(Customer.getOneData(e.getValue().getCustomer_id()).getName()));
+    }
+
+    private void openChangeDataPane() {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("change-data.fxml"));
+            AnchorPane anchorPane = new AnchorPane((Node) fxmlLoader.load());
+            anchorPaneMain.getChildren().removeFirst();
+            anchorPaneMain.getChildren().add(anchorPane);
+        }catch (IOException e1){
+            e1.printStackTrace();
+        }
     }
 }
